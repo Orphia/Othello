6b04c1423950eab44c0028a456d549187792e340
Jihanekb <karibjihane@gmail.com>
2021-01-18 19:27:41 +0100

    commit on 'master'

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 typedef char matrice[8][8];
 typedef struct { int Wh; int Bl;} couple;
 typedef struct { char username[30]; int score;} playerID;
+typedef struct { int curseur1; int curseur2;} curseur;
 
 void hori_possible(matrice M,int l, int k,char car1,char car2) {
   int i;
@@ -316,18 +317,23 @@ int plein(matrice M) {
   return true;
 }
 
-void username_base() {
+curseur username_base() {
   playerID player1,player2;
   FILE *data;
   char name[30];
-  int exist,score;
+  int exist,score, f, first_visit;
+  curseur cursor;
   data = fopen("database.txt","a+");
   if(data==NULL) {
     printf("\nError!!\n");
     exit(EXIT_FAILURE);
   }
-  printf("______________Welcome to the game!!_______________\nPLAYER 1 :Please enter your username!!\n\n");
-  do {
+  printf("______________Welcome to the game!!_______________\n\n\nPLAYER 1 :Type 1 if this is your first visit and 0 otherwise :\n");
+  scanf("%d",&first_visit);
+  printf("Please enter your username!!\n\n");
+  if(first_visit==true){
+    player1.score=0;
+    do {
       exist=false;
       scanf("%s",player1.username);
       while(!feof(data)) {
@@ -339,28 +345,94 @@ void username_base() {
             break; }
           }
      }while(exist);
-   fprintf(data,"\n%s",player1.username);
-   fclose(data);
-   data = fopen("database.txt","a+");
+    fprintf(data,"\n%s\t",player1.username);
+    cursor.curseur1=ftell(data);
+    fprintf(data,"%d",player1.score);
+    fclose(data);
+  }
+  else{
+    data = fopen("database.txt","a+");
+   if(data==NULL) {
+    printf("\nError!!\n");
+    exit(EXIT_FAILURE);
+    }
+      scanf("%s",player1.username);
+      while(!feof(data)) {
+        fscanf(data,"%s\t%d\n",name,&score);
+        if(strcmp(name,player1.username)==0) {
+            cursor.curseur1=ftell(data);
+            f=1;
+            break; }
+          }  
+      if(f!=1) {
+        player1.score=0;
+        printf("looks like this is your first time with us!!\n");
+        fprintf(data,"\n%s\t%d",player1.username,player1.score);
+        cursor.curseur1=ftell(data);
+      }
+    fclose(data);
+  }
+  data = fopen("database.txt","a+");
   if(data==NULL) {
     printf("\nError!!\n");
     exit(EXIT_FAILURE);
   }
-   printf("PLAYER 2 :Please enter your username!!\n");
-   do {
-       exist=false;
-       scanf("%s",player2.username);
-       while(!feof(data)) {
-         fscanf(data,"%s\t%d\n",name,&score);
-         if(strcmp(name,player2.username)==0) {
-             printf("exists already!! change it !!\n");
-             exist=true;
-             rewind(data);
-             break;}
-           }
-      }while(exist);
-  fprintf(data,"\n%s",player2.username);
-  fclose(data);
+  printf("______________Welcome to the game!!_______________\n\n\nPLAYER 2 :Type 1 if this is your first visit and 0 otherwise :\n");
+  scanf("%d",&first_visit);
+  printf("Please enter your username!!\n\n");
+  if(first_visit==true){
+    do {
+      player2.score=0;
+      exist=false;
+      scanf("%s",player2.username);
+      while(!feof(data)) {
+        fscanf(data,"%s\t%d\n",name,&score);
+        if(strcmp(name,player2.username)==0) {
+            printf("exists already!! change it !!\n");
+            exist=true;
+            rewind(data);
+            break; }
+          }
+     }while(exist);
+    fprintf(data,"\n%s\t%d",player2.username,player2.score);
+    cursor.curseur2=ftell(data);
+    fclose(data);
+  }
+  else{
+    data = fopen("database.txt","a+");
+   if(data==NULL) {
+    printf("\nError!!\n");
+    exit(EXIT_FAILURE);
+    }
+      scanf("%s",player2.username);
+      while(!feof(data)) {
+        fscanf(data,"%s\t%d\n",name,&score);
+        if(strcmp(name,player2.username)==0) {
+            cursor.curseur2=ftell(data);
+            f=1;
+            break; }
+          }  
+      if(f!=1) {
+        player2.score=0;
+        printf("looks like this is your first time with us!!\n");
+        fprintf(data,"\n%s\t%d",player2.username,player2.score);
+        cursor.curseur2=ftell(data);
+      }
+    fclose(data);
+  }
+  return cursor;
+}
+
+void score_update(curseur cursor,){
+  playerID player1, player2;
+  FILE *data;
+  data = fopen("database.txt","a+");
+  if(data==NULL) {
+    printf("\nError!!\n");
+    exit(EXIT_FAILURE);
+  }
+  fseek(data,cursor.curseur1,SEEK_CUR);
+  
 }
 
 int main() {
@@ -369,7 +441,10 @@ int main() {
   char car1, car2, car3, player='N';
   couple pawn;
   matrice M;
-  username_base();
+  curseur cursor;
+  cursor=username_base();
+  printf("PLAYER 1 the owner of the black pawns and PLAYER 2 the owner of the white ones.\n");
+  printf("\nNow let the game begins!!\n");
   for (i = 0; i < 8; i++) {
     for (j = 0; j < 8; j++)
       M[i][j]=' ';
@@ -447,6 +522,7 @@ int main() {
     if(s==2) break;
     iter++;
   } while(!plein(M));
+
   if(pawn.Bl<pawn.Wh) printf("White is the winner!! Congrats!!\n Black, Try next time!!");
   else if(pawn.Wh<pawn.Bl) printf("Black is the winner!! Congrats!!\n White, Try next time!!");
   else printf("it's a draw!! you both are winners and losers!!");
