158e70dd97c770316de107b760677fc02ba4d5ae
somaya287 <somiya_fourtassi@um5.ac.ma>
2021-01-25 15:23:05 +0100

    libraryAndDeclarations

---
+++ libETdecla.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define true 1
+#define false 0
+typedef char matrice[8][8];
+typedef struct { int Wh; int Bl;} couple;
+typedef struct { char username[30]; int score;} playerID;
