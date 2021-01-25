d8df5c9dfa945aeed01d2b847f39621346184d64
somaya287 <somiya_fourtassi@um5.ac.ma>
2021-01-25 15:28:46 +0100

    header2

---
+++ ColorChange.h
@@ -0,0 +1,4 @@
+void hori_color_change(matrice,int,int,char,char);
+void verti_color_change(matrice,int,int,char,char);
+void diag_color_change(matrice,int,int,char,char);
+void anti_diag_color_change(matrice,int,int,char,char); 
