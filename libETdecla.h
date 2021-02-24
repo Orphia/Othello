6f066d115f16fd38adf04e2a902b06d2203bbb2d
Karib Jihane <77389840+Jihanekb@users.noreply.github.com>
2021-02-24 01:53:41 +0100

    liv2

---
+++ libETdecla.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define true 1
+#define false 0
+typedef char matrice[8][8];
+typedef struct { int Wh; int Bl;} couple;
+typedef struct { char username[30]; int score;} playerID;
+typedef struct {int x ; int y ; int cur; } cordonnee;
+typedef struct _joueur{ playerID *info; struct _joueur *next;} joueur;
+typedef joueur *joueurliste;
---
+++ livrable1.h
@@ -0,0 +1,6 @@
+void stocker(playerID player);
+void print_moves(int tab[64][2], int iter);
+void upload(int tab[64][2],int iter);
+cordonnee reload(cordonnee cord, char partID[30]);
+void inorder_insert(joueurliste liste,joueur *newplayer);
+void topten();
