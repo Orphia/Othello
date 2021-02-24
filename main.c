6821f418514fb7709f6b0b480dfe9831f25de1a1
somaya287 <somiya_fourtassi@um5.ac.ma>
2021-02-24 01:36:16 +0100

    liv2

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "libETdecla.h"
+#include "PossibleCase.h"
+#include "ColorChange.h"
+#include "others.h"
+#include "livrable2.h"
+void main(){
+  printf("_________________Welcome to the game!!__________________\n");
+  playerID player1;
+  printf("Type ur username\n");
+  scanf("%s",player1.username);
+  menu0(player1);
+}
